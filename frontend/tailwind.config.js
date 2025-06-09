9aa681e4df24708f127c80ab520dc04791e70e4d
21jeffrey <jjkyali5@gmail.com>
2025-06-09 19:00:47 +0300

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    // add any other folders you use
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
